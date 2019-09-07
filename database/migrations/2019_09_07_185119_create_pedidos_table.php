9e68c4d854cf987cc2a0682ee5ce12fb49243634
ridabe <ridabe@uol.com.br>
2019-09-07 15:57:18 -0300

    Adicionado as migrates

---
+++ database/migrations/2019_09_07_185119_create_pedidos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePedidosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pedidos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->number('id_clients');
+            $table->number('id_products');
+            $table->number('quantidade');
+            $table->number('valor');
+            $table->number('total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pedidos');
+    }
+}
